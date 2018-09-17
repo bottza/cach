ca88936c78fd961f63e643e6d39f0bfa3d4df45b
Felix Mattick <felix.mattick@gmail.com>
2018-09-17 14:34:00 -0400

    Update README.md and code

--- README.md
+++ README.md
@@ -1,3 +1,63 @@
-# cach
+# Cach
+[![CircleCI](https://img.shields.io/circleci/project/github/arch-lord/cach/master.svg?style=flat-square)](https://circleci.com/gh/arch-lord/cach)
+[![Depfu](https://img.shields.io/depfu/arch-lord/cach.svg?style=flat-square)](https://depfu.com/repos/arch-lord/cach) 
+[![npm](https://img.shields.io/npm/v/cach.svg?style=flat-square)](https://www.npmjs.com/package/cach)
 
 A simple program to restart your NodeJS app when it exits with an error.
+
+## Installation
+
+For global use:  
+```
+$ npm install -g cach
+```
+
+For local use:
+```
+$ npm install --save cach
+```
+
+## Usage
+
+On the command line:
+```
+$ cach
+Running `npm start`...
+
+$ cach ember serve
+Running `ember serve`...
+```
+
+In a script:
+```javascript
+var cach = require("cach");
+
+// Runs npm start with cach
+cach();
+
+// Runs ember serve with cach
+cach("ember serve");
+
+// Runs npm test with cach,
+// and also logs messages
+cach("npm test", true);
+```
+
+## Testing
+
+```
+$ git clone https://github.com/arch-lord/cach
+Cloning into 'cach'...
+remote: Counting objects: 37, done.
+remote: Compressing objects: 100% (32/32), done.
+remote: Total 37 (delta 13), reused 23 (delta 3), pack-reused 0
+Unpacking objects: 100% (37/37), done.
+
+$ cd cach
+
+$ npm test
+> cach@1.0.0 test C:\Programming\cach
+> ava
+
+  4 tests passed
+```
--- main.js
+++ main.js
@@ -1,18 +1,33 @@
 var exec = require("child_process").exec;
 var chalk = require("chalk");
 
-var cach = command => {
+var cach = (command = "npm start", log = false, restarted = false) => {
+  var cmdLine = require.main === module;
+  if (!command.trim()) {
+    command = "npm start";
+  }
+  if (!restarted && (cmdLine || log)) {
+    console.log(chalk.cyan("Running `${command}`..."));
+  }
   exec(command, function(error, stdout, stderr) {
     if (error) {
-      console.log(chalk.yellow("Command `${error.cmd}` failed with exit code ${error.code}."));
-      console.log(chalk.redBright(error.toString().trim()));
-      console.log(chalk.cyan("Restarting..."));
-      console.log();
-      cach(command);
+      if (cmdLine || log) {
+        console.log(chalk.yellow("Command `${error.cmd}` failed with exit code ${error.code}."));
+        console.log(chalk.redBright(error.toString().trim()));
+        console.log(chalk.cyan("Restarting..."));
+        console.log();
+      }
+      cach(command, restarted = true);
     } else {
-      console.log(chalk.green("Command `${command} exited cleanly."));
-      console.log(chalk.cyan("Exiting..."));
-      process.exit(0);
+      if (cmdLine || log) {
+        console.log(chalk.green("Command `${command} exited cleanly."));
+        console.log(chalk.cyan("Exiting..."));
+      }
+      if (cmdLine) {
+        process.exit(0);
+      } else {
+        return;
+      }
     }
   });
 };
@@ -20,7 +35,7 @@ var cach = command => {
 if (require.main === module) {
   var args = process.argv.slice(2);
   if (args.length == 0) {
-    cach("npm start");
+    cach();
   } else {
     cach(args.join(" "));
   }
